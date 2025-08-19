68515929ae5adb7e453773f4f6b0dbcb36e85343
Kishore Korathaluri <kkorathaluri@cribl.io>
2025-08-19 17:45:12 +1000

    fixing test.

--- examples/regex/main.tf
+++ examples/regex/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    criblio = {
-      source = "criblio/criblio"
-    }
-  }
-}
-
-provider "criblio" {
-  # Configuration options
-  #server_url ="https://app.cribl-playground.cloud/organizations/beautiful-nguyen-y8y4azd/workspaces/main/app/api/v1"
-  organization_id = "beautiful-nguyen-y8y4azd"
-  workspace_id = "main"
-  server_url = "https://app.cribl-playground.cloud"
-}
 resource "criblio_regex" "my_regex" {
   description = "test_regex_2"
   group_id    = "default"
