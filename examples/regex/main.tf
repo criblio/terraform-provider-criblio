c05d2b1be1ad0a7c3c5b341b3a88aec7f30643f2
Kishore Korathaluri <kkorathaluri@cribl.io>
2025-08-19 17:42:07 +1000

    Adding examples for regex resource.

--- examples/regex/main.tf
+++ examples/regex/main.tf
@@ -1,7 +1,22 @@
+terraform {
+  required_providers {
+    criblio = {
+      source = "criblio/criblio"
+    }
+  }
+}
+
+provider "criblio" {
+  # Configuration options
+  #server_url ="https://app.cribl-playground.cloud/organizations/beautiful-nguyen-y8y4azd/workspaces/main/app/api/v1"
+  organization_id = "beautiful-nguyen-y8y4azd"
+  workspace_id = "main"
+  server_url = "https://app.cribl-playground.cloud"
+}
 resource "criblio_regex" "my_regex" {
-  description = "test"
+  description = "test_regex_2"
   group_id    = "default"
-  id          = "test_regex"
+  id          = "test_regex_2"
   lib         = "custom"
   regex       = "/\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b/"
   sample_data = "192.168.1.1, 10.0.0.1, 172.16.0.1"
