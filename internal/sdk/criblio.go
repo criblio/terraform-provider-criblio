ca359628010230b8241712de041beeee7fd8c66d
kkorathaluri <kkorathaluri@cribl.io>
2025-10-29 12:47:40 +1100

    Adding on-perm support for terraform provider. (#65)
    * Adding on-perm support for terraform provider.
    
    * fixing gofmt.
    
    * removing customer managed text.

--- internal/sdk/criblio.go
+++ internal/sdk/criblio.go
@@ -358,9 +358,9 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *CriblIo {
 	sdk := &CriblIo{
-		SDKVersion: "1.18.8",
+		SDKVersion: "1.18.10",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:  "speakeasy-sdk/terraform 1.18.8 2.723.11 4.14.0 github.com/criblio/terraform-provider-criblio/internal/sdk",
+			UserAgent:  "speakeasy-sdk/terraform 1.18.10 2.723.11 4.14.0 github.com/criblio/terraform-provider-criblio/internal/sdk",
 			ServerList: ServerList,
 			ServerVariables: map[string]map[string]string{
 				"cloud": {
