7768f93831966b5cc322f2b9e24959096e73b8f9
Kayleigh Traister <ktraister@cribl.io>
2025-11-03 09:56:45 -0500

    terraform fmt and fixed my githooks

--- examples/resources/criblio_pack_lookups/import-by-string-id.tf
+++ examples/resources/criblio_pack_lookups/import-by-string-id.tf
@@ -2,7 +2,7 @@ import {
   to = criblio_pack_lookups.my_criblio_pack_lookups
   id = jsonencode({
     group_id = "myExistingGroupId"
-    id = "myUniqueLookupIdToCRUD"
-    pack = "myExistingPackId"
+    id       = "myUniqueLookupIdToCRUD"
+    pack     = "myExistingPackId"
   })
 }
