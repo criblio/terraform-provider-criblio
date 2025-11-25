0a1aa1dcb74ec48a931ca9b9fdbe1c61a23c6db4
ktraister-cribl <ktraister@cribl.io>
2025-11-25 10:26:44 -0500

    Bugfix/ktraister/fix internal types (#82)
    * fixing generation broken during types refactor
    
    * fixing generation broken during types refactor
    
    * fixing generation broken during types refactor
    
    * fixed missing stucts

--- internal/sdk/internal/hooks/types.go
+++ internal/sdk/internal/hooks/types.go
@@ -1,13 +1,8 @@
 package hooks
 
 import (
-	"context"
-	"net/http"
 	"sync"
 	"time"
-
-	"github.com/criblio/terraform-provider-criblio/internal/sdk/internal/config"
-	"golang.org/x/sync/singleflight"
 )
 
 type TokenInfo struct {
@@ -22,35 +17,6 @@ type CriblTerraformHook struct {
 	baseURL     string
 	orgID       string
 	workspaceID string
-}
-
-type session struct {
-	Credentials *credentials
-	Token       string
-	ExpiresAt   *int64
-	Scopes      []string
-}
-
-type tokenResponse struct {
-	AccessToken string `json:"access_token"`
-	TokenType   string `json:"token_type"`
-	ExpiresIn   *int64 `json:"expires_in"`
-}
-
-type credentials struct {
-	ClientID             string
-	ClientSecret         string
-	TokenURL             string
-	Scopes               []string
-	AdditionalProperties map[string]string
-}
-
-type clientCredentialsHook struct {
-	client   HTTPClient
-	sessions sync.Map
-
-	// sessionsGroup prevents concurrent token refreshes.
-	sessionsGroup *singleflight.Group
 }
 
 type CriblConfig struct {
@@ -68,63 +34,4 @@ type CriblConfig struct {
 
 type CriblConfigFile struct {
 	Profiles map[string]CriblConfig `json:"profiles"`
-}
-
-type FailEarly struct {
-	Cause error
-}
-
-// HTTPClient provides an interface for supplying the SDK with a custom HTTP client
-type HTTPClient interface {
-	Do(req *http.Request) (*http.Response, error)
-}
-
-type HookContext struct {
-	SDK              any
-	SDKConfiguration config.SDKConfiguration
-	BaseURL          string
-	Context          context.Context
-	OperationID      string
-	OAuth2Scopes     []string
-	SecuritySource   func(context.Context) (interface{}, error)
-}
-
-type BeforeRequestContext struct {
-	HookContext
-}
-
-type AfterSuccessContext struct {
-	HookContext
-}
-
-type AfterErrorContext struct {
-	HookContext
-}
-
-// sdkInitHook is called when the SDK is initializing. The hook can modify and return a new baseURL and HTTP client to be used by the SDK.
-type sdkInitHook interface {
-	SDKInit(baseURL string, client HTTPClient) (string, HTTPClient)
-}
-
-// beforeRequestHook is called before the SDK sends a request. The hook can modify the request before it is sent or return an error to stop the request from being sent.
-type beforeRequestHook interface {
-	BeforeRequest(hookCtx BeforeRequestContext, req *http.Request) (*http.Request, error)
-}
-
-// afterSuccessHook is called after the SDK receives a response. The hook can modify the response before it is handled or return an error to stop the response from being handled.
-type afterSuccessHook interface {
-	AfterSuccess(hookCtx AfterSuccessContext, res *http.Response) (*http.Response, error)
-}
-
-// afterErrorHook is called after the SDK encounters an error, or a non-successful response. The hook can modify the response if available otherwise modify the error.
-// All afterErrorHook hooks are called and returning an error won't stop the other hooks from being called. But if you want to stop the other hooks from being called, you can return a FailEarly error wrapping your error.
-type afterErrorHook interface {
-	AfterError(hookCtx AfterErrorContext, res *http.Response, err error) (*http.Response, error)
-}
-
-type Hooks struct {
-	sdkInitHooks      []sdkInitHook
-	beforeRequestHook []beforeRequestHook
-	afterSuccessHook  []afterSuccessHook
-	afterErrorHook    []afterErrorHook
 }
