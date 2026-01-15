ad339ef87f4fc98df23ba5134f8ea21dbdc0e4fb
Kishore Korathaluri <kkorathaluri@cribl.io>
2026-01-16 08:23:26 +1100

    updating tests for on-prem.

--- tests/acceptance/mappings_test.go
+++ tests/acceptance/mappings_test.go
@@ -1,7 +1,6 @@
 package tests
 
 import (
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/config"
@@ -9,9 +8,6 @@ import (
 )
 
 func TestMappings(t *testing.T) {
-	if os.Getenv("DEPLOYMENT") == "onprem" {
-		t.Skip("Skipping resource for On-Prem deployments as it is not supported onprem")
-	}
 	t.Run("plan-diff", func(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			ProtoV6ProviderFactories: providerFactory,
