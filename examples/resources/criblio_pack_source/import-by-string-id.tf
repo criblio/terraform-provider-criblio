f1c7b4d81f35652564d75894570215f34b42e6bc
Kishore Korathaluri <kkorathaluri@cribl.io>
2026-02-16 15:07:06 +1100

    fixing terraform fmt.

--- examples/resources/criblio_pack_source/import-by-string-id.tf
+++ examples/resources/criblio_pack_source/import-by-string-id.tf
@@ -2,7 +2,7 @@ import {
   to = criblio_pack_source.my_criblio_pack_source
   id = jsonencode({
     group_id = "Cribl"
-    id = "pack-input-hec"
-    pack = "observability-pack"
+    id       = "pack-input-hec"
+    pack     = "observability-pack"
   })
 }
--- examples/resources/criblio_secret/import-by-string-id.tf
+++ examples/resources/criblio_secret/import-by-string-id.tf
@@ -2,6 +2,6 @@ import {
   to = criblio_secret.my_criblio_secret
   id = jsonencode({
     group_id = "default"
-    id = "secret-s3"
+    id       = "secret-s3"
   })
 }
